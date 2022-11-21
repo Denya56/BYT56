0f1dfbc5e31657f2885cd971b5f11d830d4391c2
Denya56 <56lkj546@gmail.com>
2022-11-21 17:43:56 +0100

    added object pool

--- Memento/Program.cs
+++ Memento/Program.cs
@@ -1,4 +1,5 @@
-﻿using Memento.SourceFiles.Mediator;
+﻿using DesignPatterns.SourceFiles.ObjectPool;
+using Memento.SourceFiles.Mediator;
 using Memento.SourceFiles.Memento;
 
 #region Memento
@@ -29,4 +30,40 @@ chatroom.Participants.Add(owner);
 guest.Send("Hello! Did you see a new product");
 owner.Send("No, not yet");
 guest.Send("Oh ok");
+#endregion
+
+#region Object pool
+
+Console.WriteLine("\n-----Object pool Pattern Example-----\n");
+Factory factory = new Factory();
+
+Console.WriteLine("Current amount of objects in pool: " + Factory.objPool.Count);
+Console.WriteLine("Current amount of acive equipment: " + Equipment.equipmentCounter);
+Equipment eq1 = factory.GetEquipment();
+Equipment eq2 = factory.GetEquipment();
+Equipment eq3 = factory.GetEquipment();
+Console.WriteLine("Current amount of objects in pool: " + Factory.objPool.Count);
+Console.WriteLine("Current amount of acive equipment: " + Equipment.equipmentCounter);
+Equipment eq4 = factory.GetEquipment();
+Equipment eq5 = factory.GetEquipment();
+Equipment eq6 = factory.GetEquipment();
+Console.WriteLine("Current amount of objects in pool: " + Factory.objPool.Count);
+Console.WriteLine("Current amount of acive equipment: " + Equipment.equipmentCounter);
+factory.DeleteEquipment(eq1);
+factory.DeleteEquipment(eq2);
+factory.DeleteEquipment(eq3);
+Console.WriteLine("Current amount of objects in pool: " + Factory.objPool.Count);
+Console.WriteLine("Current amount of acive equipment: " + Equipment.equipmentCounter);
+Equipment eq7 = factory.GetEquipment();
+Equipment eq8 = factory.GetEquipment();
+Console.WriteLine("Current amount of objects in pool: " + Factory.objPool.Count);
+Console.WriteLine("Current amount of acive equipment: " + Equipment.equipmentCounter);
+Equipment eq9 = factory.GetEquipment();
+Equipment eq10 = factory.GetEquipment();
+Console.WriteLine("Current amount of objects in pool: " + Factory.objPool.Count);
+Console.WriteLine("Current amount of acive equipment: " + Equipment.equipmentCounter);
+factory.DeleteEquipment(eq4);
+Console.WriteLine("Current amount of objects in pool: " + Factory.objPool.Count);
+Console.WriteLine("Current amount of acive equipment: " + Equipment.equipmentCounter);
+
 #endregion
---
+++ Memento/SourceFiles/ObjectPool/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SourceFiles.ObjectPool
+{
+    class Equipment
+    {
+        public static int equipmentMaxSize = 3;
+        public static int equipmentCounter = 0;
+        public Equipment()
+        {
+            Console.WriteLine("Active equipment was created.");
+            ++equipmentCounter;
+        }
+        public Equipment(String equipmentForPool)
+        {
+            Console.WriteLine("Equipment was added to pool.");
+        }
+    }
+}
